13fc3b06d6fcf516b1caa566edd391f355d78181
Rahul <amaleexxii@gmail.com>
2025-01-21 17:38:06 +0530

    configure Redux store with authSlice and wrap App component in Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './redux/authSlice'
 import App from './App.tsx'
+import './index.css'
+
+const store = configureStore({
+  reducer :{
+    auth : authReducer
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
 )
