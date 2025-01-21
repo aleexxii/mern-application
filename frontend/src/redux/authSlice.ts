51a906428bed39e922d8939f992c9c59d474e729
Rahul <amaleexxii@gmail.com>
2025-01-21 16:50:31 +0530

    implement authSlice with initial state and loginSuccess reducer

--- frontend/src/redux/authSlice.ts
+++ frontend/src/redux/authSlice.ts
@@ -1,1 +1,31 @@
 
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+
+interface AuthState {
+    isAuthenticated : boolean;
+    userRole : string | null;
+    userInfo : object | null | any;
+}
+
+const initialState : AuthState = {
+    isAuthenticated : false,
+    userRole : null,
+    userInfo : null
+}
+
+const authSlice = createSlice({
+    name : 'auth',
+    initialState,
+    reducers : {
+        loginSuccess : (state, action : PayloadAction<{userRole : string;userInfo : any }>) => {
+            state.isAuthenticated = true;
+            state.userRole = action.payload.userRole;
+            state.userInfo = action.payload.userInfo
+        }
+    }
+})
+
+export const { loginSuccess} = authSlice.actions;
+export default authSlice.reducer
+
