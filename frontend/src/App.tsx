21f63a89e72617f3cfedfec340a0bdaca3d0a276
Rahul <amaleexxii@gmail.com>
2025-01-21 17:28:52 +0530

    implement routing in App component with ProtectedRoute for user role-based access

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,27 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useSelector, UseSelector } from "react-redux"
+import LoginPage from "./pages/LoginPage"
+import ProtectedRoute from "./components/ProtectedRoute"
+import AdminDashboard from "./pages/AdminDashboard"
+import UserRoutes from "./pages/UserRoutes"
 
-function App() {
 
-  return (
-    <>
-      
-    </>
-  )
+const App : React.FC = () => {
+  const {userRole} = useSelector((state : any) => state.auth)
+    return(
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              {userRole == 'admin' ? <AdminDashboard /> : <UserRoutes />}
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </Router>
+    )
 }
 
 export default App
