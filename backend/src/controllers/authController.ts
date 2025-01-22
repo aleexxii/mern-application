6398e355c27129f5baa2d5666e0b6144811e8de1
Rahul <amaleexxii@gmail.com>
2025-01-22 18:35:39 +0530

    refactor signUp and login functions in authController; improve code readability and enhance response structure

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -4,46 +4,62 @@ import User from "../models/User";
 import { generateToken } from "../utils/generateToken";
 import { errorHandler } from "../middleware/errorHandler";
 
-export const signUp = async (req: Request, res: Response, next : NextFunction) :Promise<void> => {
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   const { name, email, password, role } = req.body;
 
   try {
-
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       return next(errorHandler(400, "User already exists"));
     }
 
-
     const user = new User({ name, email, password });
     await user.save();
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
-export const login = async (req : Request , res : Response, next : NextFunction) => {
-  const { email, password } = req.body
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
 
-  try{
-    const user = await User.findOne({email})
-
-    if(!user){
-      return next(errorHandler(401, 'User not found'))
+    if (!user) {
+      return next(errorHandler(401, "User not found"));
     }
 
-    const isPasswordMatch = await bcrypt.compare(password, user.password)
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordMatch){
-      return next(errorHandler(401,'Invalid password'))
+    if (!isPasswordMatch) {
+      return next(errorHandler(401, "Invalid password"));
     }
-    const token = generateToken(user)
+    const token = generateToken(user);
 
-    res.status(200).json({ token, role : user.role })
-  }catch(err){
-    next(err)
+    const userObject = user.toObject();
+
+    const { password: userPassword, ...userInfo } = userObject;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({ userInfo });
+      
+  } catch (err) {
+    next(err);
   }
-}
+};
